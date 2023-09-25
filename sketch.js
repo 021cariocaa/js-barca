fd36af882e84535938c405d72ca8a180862e7465
021cariocaa <145365029+021cariocaa@users.noreply.github.com>
2023-09-25 11:50:42 -0300

    carregando arquivos html, css e js

---
+++ sketch.js
@@ -0,0 +1,101 @@
+Cookies
+The p5.js Editor uses cookies. Some are essential to the website functionality and allow you to manage an account and preferences. Others are not essential—they are used for analytics and allow us to learn more about our community. We never sell this data or use it for advertising. You can decide which cookies you would like to allow, and learn more in our Privacy Policy.
+
+
+Allow All
+
+Allow Essential
+Arquivo
+Editar
+Esboço
+Ajuda
+Português
+Olá, giulia.franco.silva!
+
+
+
+Auto-atualizar
+
+barça
+por giulia.franco.silva
+
+
+
+sketch.js
+Salvo: 3 meses atrás
+1
+function setup() {
+2
+​
+3
+​
+4
+createCanvas(400, 400);
+5
+​
+6
+background("black");
+7
+​
+8
+}
+9
+​
+10
+function draw() {
+11
+​
+12
+textAlign(CENTER, CENTER);
+13
+​
+14
+textSize(38);
+15
+  
+16
+  textFont("Tilt Prism");
+17
+​
+18
+fill("red");
+19
+​
+20
+text("YO", width / 2, height - 250);
+21
+​
+22
+fill("blue");
+23
+​
+24
+text("AMO", width / 2, height - 200);
+25
+​
+26
+fill("red");
+27
+​
+28
+text("BARCELONA", width / 2, height - 150);
+29
+​
+30
+ fill("blue");
+31
+  
+32
+  text("PEDRI Y GAVI",width / 2, height - 100);
+33
+​
+34
+}
+Não há mensagens de Lint
+Linha atual
+
+Terminal
+Limpar
+
+​
+Prévia
